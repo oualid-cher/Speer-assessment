4e95cbd6bff9f477d57db6ecee818ad73a0aba55
oualid-cher <ahmedoualidcherkaoui@gmail.com>
2021-07-26 13:19:53 +0100

    adding images fields to yellow

--- src/components/yellow/yellow.js
+++ src/components/yellow/yellow.js
@@ -4,14 +4,19 @@ import "./yellow.css"
 const Yellow = () => {
     return (
         <Container fluid id="yellow">
-            <div>
-                <div id="img-div">
-                    <h3 id="img2">IMAGE</h3>
-                    <h3 id="img1">IMAGE</h3>
-                    <div id="reveal">
-                        <h3 id="reveal-txt">REVEAL</h3>
-                    </div>
+            <div id="img-div">
+                <h3 id="img2">IMAGE</h3>
+                <h3 id="img1">IMAGE</h3>
+                <div id="reveal">
+                    <h3 id="reveal-txt">REVEAL</h3>
                 </div>
+            </div>
+            <div id="yellow-txt">
+                <h3>FRONT ROW SEATS</h3>
+                <p>Experience concerts up close and personal.</p>
+            </div>
+            <div id="img3-div">
+                <h3 >IMAGE</h3>
             </div>
         </Container>
     )
